bbd291a1af1a6e7d979fbb87b872592737111c6a
nathy10carvalh0 <nathy10carvalho@gmail.com>
2023-09-07 20:12:42 -0300

    aula-3 vetores e objetos

---
+++ aula-3/script.js
@@ -0,0 +1,12 @@
+//O que são vetores e arrays
+/*
+// como declarar um array
+let array = ['string', 1, true];
+console.log(array);
+*/
+//pode guardar varios tipos de dados
+let array = ['string', 1, true, ['array1'], ['array2'], ['array3'], ['array4']];
+console.log(array[0]);
+
+//forEach
+array.forEach()
