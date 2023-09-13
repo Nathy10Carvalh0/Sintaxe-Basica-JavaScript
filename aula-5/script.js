758a21f549923631470be9c7739dacde99366ce5
nathy10carvalh0 <nathy10carvalho@gmail.com>
2023-09-13 07:36:41 -0300

    aula-5 update

---
+++ aula-5/script.js
@@ -0,0 +1,12 @@
+function funcao(){
+    console.log('tudo certo!');
+}
+
+funcao();
+
+//função com paramentro
+function mensagem(primeiro, segundo){
+    console.log(primeiro, segundo);
+}
+
+mensagem('tudo certo', 'jovem');
